a5390bb76f923cb96706b098c74c5dc172802b64
fadli1003 <huzhain@gmail.com>
2026-01-16 13:01:52 +0700

    edit config middleware untuk bearer

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -60,11 +60,17 @@ class AuthController extends Controller
         }
     }
 
-    public function logout()
+    public function logout(Request $request)
     {
         try{
             $user = Auth::user();
-            $user->currentAccessToken()->delete();
+            // if($user->currentAccessToken()){
+              $user->currentAccessToken()->delete();
+            // }else{
+            //   auth()->guard('web')->logout();
+            //   $request->session()->invalidate();
+            //   $request->session()->regenerateToken();
+            // }
 
             return response()->json([
                 'message' => 'Logout succesfully.',
