b5be033bd6d9dfd804bdbf455dbef8b93278a8d7
fadli1003 <huzhain@gmail.com>
2026-01-20 16:11:42 +0700

    .

--- src/router/index.js
+++ src/router/index.js
@@ -51,31 +51,3 @@ const router = createRouter({
 })
 
 export default router
-
-// import { createRouter, createWebHistory } from 'vue-router'
-// import { useAuthStore } from '@/stores/auth'
-
-// const routes = [
-//   { path: '/', component: () => import('@/views/HomeView.vue') },
-//   { path: '/login', component: () => import('@/views/LoginView.vue') },
-//   {
-//     path: '/admin',
-//     component: () => import('@/views/AdminView.vue'),
-//     meta: { requiresAuth: true }
-//   }
-// ]
-
-// const router = createRouter({
-//   history: createWebHistory(),
-//   routes
-// })
-
-// // Navigation guard
-// router.beforeEach((to) => {
-//   const auth = useAuthStore()
-//   if (to.meta.requiresAuth && !auth.isAuthenticated) {
-//     return '/login'
-//   }
-// })
-
-// export default router
